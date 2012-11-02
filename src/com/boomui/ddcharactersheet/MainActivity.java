f8cf4fd46f32c322ef93528b30267c8a37032dcd
rogin012 <rogin012@umn.edu>
2012-11-02 15:00:03 -0500

    Made app fullscreen.

--- src/com/boomui/ddcharactersheet/MainActivity.java
+++ src/com/boomui/ddcharactersheet/MainActivity.java
@@ -3,13 +3,19 @@ package com.boomui.ddcharactersheet;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		setContentView(R.layout.activity_main);
     }
 
     @Override
