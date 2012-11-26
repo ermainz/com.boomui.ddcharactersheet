122fb44c058a3d47d09fbae012604c0034e75fd6
oconn399 <oconn399@umn.edu>
2012-11-25 20:09:14 -0600

    implemented info tab. Character picture displays, but cannot be changed.

--- src/com/boomui/ddcharactersheet/InfoTabFragment.java
+++ src/com/boomui/ddcharactersheet/InfoTabFragment.java
@@ -1,20 +1,64 @@
 package com.boomui.ddcharactersheet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.annotation.SuppressLint;
 import android.app.*;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.*;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class InfoTabFragment extends Fragment{
+	FragmentCommunicator com;
+	View mainView;
+	Map<CharacterDataKey, Integer> editableFields;
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 	}
 	
+	public void onAttach(Activity activity){
+		super.onAttach(activity);
+		com = (FragmentCommunicator) activity;
+	}
+	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-		return inflater.inflate(R.layout.info_tab_fragment_layout, container, false);
+		mainView = inflater.inflate(R.layout.info_tab_fragment_layout, container, false);
+		
+		editableFields = new HashMap<CharacterDataKey, Integer>();
+		editableFields.put(CharacterDataKey.NAME, R.id.character_name);
+		editableFields.put(CharacterDataKey.INFO_CLASS_AND_LEVEL, R.id.class_and_level);
+		editableFields.put(CharacterDataKey.INFO_RACE, R.id.race);
+		editableFields.put(CharacterDataKey.INFO_PATRON_DEITY, R.id.patron_deity);
+		editableFields.put(CharacterDataKey.INFO_ALIGNMENT, R.id.alignment);
+		editableFields.put(CharacterDataKey.INFO_SIZE, R.id.sizeof_char);
+		editableFields.put(CharacterDataKey.INFO_GENDER, R.id.gender);
+		editableFields.put(CharacterDataKey.INFO_AGE, R.id.age);
+		editableFields.put(CharacterDataKey.INFO_CHAR_DESCR, R.id.character_description);
+		
+		for (CharacterDataKey key : editableFields.keySet()) {
+			EditText txtBox = (EditText) mainView.findViewById( editableFields.get(key) );
+			String txtVal = com.loadData(key);
+			if (txtVal == null) {
+				txtVal = "";
+			}
+			txtBox.setText(txtVal);
+		}
+		
+		return mainView;
+	}
+	
+	public void onPause() {
+		super.onPause();
+		for (CharacterDataKey key : editableFields.keySet()) {
+			EditText txtBox = (EditText) mainView.findViewById( editableFields.get(key) );
+			com.saveData( key, txtBox.getText().toString() );
+		}
+		
 	}
 }
