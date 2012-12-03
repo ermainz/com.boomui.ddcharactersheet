3a5940788b46b38d4b609b03f715c4f9460739c1
rogin012 <rogin012@umn.edu>
2012-12-02 21:14:13 -0600

    Keyboard closes on switching tabs (though still not on opening activity).

--- src/com/boomui/ddcharactersheet/FeatsTabFragment.java
+++ src/com/boomui/ddcharactersheet/FeatsTabFragment.java
@@ -2,10 +2,12 @@ package com.boomui.ddcharactersheet;
 
 import android.annotation.SuppressLint;
 import android.app.*;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.*;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 
 public class FeatsTabFragment extends Fragment{
@@ -14,6 +16,14 @@ public class FeatsTabFragment extends Fragment{
 		super.onCreate(savedInstanceState);
 	}
 	
+	public void onAttach(Activity activity){
+		super.onAttach(activity);
+
+		//This code hides the keyboard
+		InputMethodManager inputManager = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+		inputManager.hideSoftInputFromWindow((null == activity.getCurrentFocus()) ? null : activity.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+	}
+
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		return inflater.inflate(R.layout.feats_tab_fragment_layout, container, false);
 	}
